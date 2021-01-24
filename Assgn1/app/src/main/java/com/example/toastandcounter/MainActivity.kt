419b8453f59a640cc2c5f4082ebb3cefab578260
marcellpaganini <59565705+marcellpaganini@users.noreply.github.com>
2021-01-24 15:36:45 -0400

    added counter

--- Assgn1/app/src/main/java/com/example/toastandcounter/MainActivity.kt
+++ Assgn1/app/src/main/java/com/example/toastandcounter/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.toastandcounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnUp: Button = findViewById(R.id.btn_up)
+        btnUp.setOnClickListener { countUpClick() }
+        val btnDown: Button = findViewById(R.id.btn_down)
+        btnDown.setOnClickListener { countDownClick() }
+    }
+
+    private var counter: Int = 0
+    private fun countUpClick(){
+        counter +=1
+        val textView: TextView = findViewById(R.id.textView)
+        textView.text = counter.toString()
+    }
+
+    private fun countDownClick(){
+        counter -=1
+        val textView: TextView = findViewById(R.id.textView)
+        textView.text = counter.toString()
     }
 }
